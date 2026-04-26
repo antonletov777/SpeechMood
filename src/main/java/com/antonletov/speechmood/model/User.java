8468725bacd54a8ada75e52f3904f8edc2d696a9
antonletov777 <antonletov-777@ya.ru>
2026-04-26 15:59:52 +0400

    Поработал со Spring Security, добавил возможность аутентификации посредством JWT токенов, также реализовал концепцию blacklist, добавил класс BlacklistToken, BlacklistTokenRepository, JWTService, JWTAuthenticationFilter, немного скорректировал класс User

--- src/main/java/com/antonletov/speechmood/model/User.java
+++ src/main/java/com/antonletov/speechmood/model/User.java
@@ -1,16 +1,23 @@
 package com.antonletov.speechmood.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import java.time.LocalDateTime;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,5 +37,33 @@ public class User {
     )
     private Set<User> friends;
 
-    // Геттеры и сеттеры
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/com/antonletov/speechmood/repository/BlacklistedTokenRepository.java
@@ -0,0 +1,10 @@
+package com.antonletov.speechmood.repository;
+
+import com.antonletov.speechmood.model.BlacklistedToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, Long> {
+    boolean existsByToken(String token);
+}
